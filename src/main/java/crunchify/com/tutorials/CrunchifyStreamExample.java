70531a431013e6d47e2376deaa408767099eb5c8
Siri KV <shireesha.kvc@gmail.com>
2022-03-03 22:35:26 -0600

    inital commit

---
+++ src/main/java/crunchify/com/tutorials/CrunchifyStreamExample.java
@@ -0,0 +1,7 @@
+package crunchify.com.tutorials;
+
+public class CrunchifyStreamExample {
+    public static void main(String args[]){
+        System.out.println("Hey.. khghjgkhjkjkjkjThanks for visiting Crunchify.com. We are now pushing file to github from IntelliJ IDEA.");
+    }
+}
